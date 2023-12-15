11823cf94574ac4f55426ffbad48914a47c07258
Павел Пономарев <permanganatoff@gmail.com>
2023-12-15 18:06:23 +0400

    Add find dotenv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,8 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
