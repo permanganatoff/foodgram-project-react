8e166a2b1af2ab6274a3abe2ce14cb1718da80eb
Павел Пономарев <permanganatoff@gmail.com>
2023-12-16 13:23:59 +0400

    Change admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -2,132 +2,129 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.utils.safestring import mark_safe
 
-from recipes.constants import MAX_VALUE, MIN_VALUE
+from recipes.constants import MAX_INGREDIENTS
 
 from .models import (AmountIngredient, Favorite, Ingredient, Recipe,
                      ShoppingCart, Tag)
 
-admin.site.site_header = 'Администрирование Foodgram'
-admin.site.unregister(Group)
-
 
 class IngredientInline(admin.TabularInline):
     model = AmountIngredient
-    extra = 1
-    min_num = MIN_VALUE
-    max_num = MAX_VALUE
-    validate_min = True
-    validate_max = True
+    extra = MAX_INGREDIENTS
 
 
-@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
-        'author',
-        'get_image',
-        'cooking_time',
-        'count_favorites',
-        'get_ingredients',
+        "name",
+        "author",
+        "get_image",
+        "cooking_time",
+        "count_favorites",
+        "get_ingredients",
     )
     fields = (
         (
-            'name',
-            'cooking_time',
+            "name",
+            "cooking_time",
         ),
         (
-            'author',
-            'tags',
+            "author",
+            "tags",
         ),
-        ('text',),
-        ('image',),
+        ("text",),
+        ("image",),
     )
-    raw_id_fields = ('author',)
+    raw_id_fields = ("author",)
     search_fields = (
-        'name',
-        'author__username',
-        'tags__name',
+        "name",
+        "author__username",
+        "tags__name",
     )
-    list_filter = ('name', 'author__username', 'tags__name')
+    list_filter = ("name", "author__username", "tags__name")
 
     inlines = (IngredientInline,)
     save_on_top = True
-    empty_value_display = '-пусто-'
+    empty_value_display = "-empty-"
 
-    @admin.display(description='Фотография')
+    @admin.display(description="Photo")
     def get_image(self, obj):
         return mark_safe(f"<img src={obj.image.url} width='80' hieght='30'")
 
-    @admin.display(description='В избранном')
+    @admin.display(description="In favorites")
     def count_favorites(self, obj):
         return obj.recipes_favorite_related.count()
 
-    @admin.display(description='Ингредиенты')
+    @admin.display(description="Ingredients")
     def get_ingredients(self, obj):
-        return ', '.join(
+        return ", ".join(
             ingredient.name for ingredient in obj.ingredients.all())
 
-    list_display_links = ('name', 'author')
+    list_display_links = ("name", "author")
 
 
-@admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
-        'measurement_unit',
+        "name",
+        "measurement_unit",
     )
-    search_fields = ('name',)
-    list_filter = ('name',)
-    empty_value_display = '-пусто-'
+    search_fields = ("name",)
+    list_filter = ("name",)
+    empty_value_display = "-empty-"
 
     save_on_top = True
 
 
-@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
-        'color',
-        'slug',
+        "name",
+        "color",
+        "slug",
     )
-    empty_value_display = '-пусто-'
-    search_fields = ('name', 'color')
-    list_display_links = ('name', 'color')
+    empty_value_display = "-empty-"
+    search_fields = ("name", "color")
+    list_display_links = ("name", "color")
     save_on_top = True
 
 
-@admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = (
-        'user',
-        'recipe',
+        "user",
+        "recipe",
     )
-    search_fields = ('user__username', 'recipe__name')
-    empty_value_display = '-пусто-'
-    list_display_links = ('user', 'recipe')
+    search_fields = ("user__username", "recipe__name")
+    empty_value_display = "-empty-"
+    list_display_links = ("user", "recipe")
     save_on_top = True
 
 
-@admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = (
-        'user',
-        'recipe',
-        'date_added',
+        "user",
+        "recipe",
+        "date_added",
     )
-    search_fields = ('user__username', 'recipe__name')
-    empty_value_display = '-пусто-'
-    list_display_links = ('user', 'recipe')
+    search_fields = ("user__username", "recipe__name")
+    empty_value_display = "-empty-"
+    list_display_links = ("user", "recipe")
     save_on_top = True
 
 
-@admin.register(AmountIngredient)
 class AmountIngredientAdmin(admin.ModelAdmin):
     list_display = (
-        'recipe',
-        'ingredient',
-        'amount',
+        "recipe",
+        "ingredient",
+        "amount",
     )
-    empty_value_display = '-пусто-'
-    list_display_links = ('recipe', 'ingredient')
+    empty_value_display = "-empty-"
+    list_display_links = ("recipe", "ingredient")
     save_on_top = True
+
+
+admin.site.site_header = "Foodgram Administration"
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(AmountIngredient, AmountIngredientAdmin)
+admin.site.unregister(Group)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,41 +4,43 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Subscription, User
 
 
-@admin.register(User)
 class UserAdmin(UserAdmin):
     list_display = (
-        'username',
-        'id',
-        'email',
-        'first_name',
-        'last_name',
-        'is_active',
-        'get_recipes_count',
-        'get_subscribers_count',
+        "username",
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "get_recipes_count",
+        "get_subscribers_count",
     )
-    list_filter = ('email', 'first_name', 'is_active')
+    list_filter = ("email", "first_name", "is_active")
     search_fields = (
-        'username',
-        'email',
+        "username",
+        "email",
     )
-    empty_value_display = '-пусто-'
+    empty_value_display = "-empty-"
 
     save_on_top = True
 
-    @admin.display(description='Количество рецептов')
+    @admin.display(description="Количество рецептов")
     def get_recipes_count(self, obj):
         return obj.recipes.count()
 
-    @admin.display(description='Количество подписчиков')
+    @admin.display(description="Количество подписчиков")
     def get_subscribers_count(self, obj):
         return obj.author.count()
 
 
-@admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'author')
-    search_fields = ('user', 'author')
-    list_filter = ('user', 'author')
-    empty_value_display = '-пусто-'
+    list_display = ("id", "user", "author")
+    search_fields = ("user", "author")
+    list_filter = ("user", "author")
+    empty_value_display = "-empty-"
 
     save_on_top = True
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
